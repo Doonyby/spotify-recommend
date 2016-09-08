7080255825665e19f538e7dbf3a4f08596d777c8
danriding <doonyby@gmail.com>
2016-09-08 22:24:05 +0000

    top tracks working

--- spotify-recommend/server.js
+++ spotify-recommend/server.js
@@ -31,9 +31,20 @@ var getRelatedArtists = function(id) {
     return emitter;
 }
 
-
-// https://api.spotify.com/v1/artists/{id}/related-artists
-// /search?q=<name>&limit=1&type=artist
+var getRelatedTopTracks = function(artist, cb) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/artists/' + artist.id + '/top-tracks?country=US')
+           .end(function(response) {
+               if (response.ok) {
+                   artist.tracks = response.body.tracks;
+                   cb();
+               }
+               else {
+                   emitter.emit('error', response.code);
+               }
+           });
+    return emitter;
+}
 
 var app = express();
 app.use(express.static('public'));
@@ -48,22 +59,37 @@ app.get('/search/:name', function(req, res) {
     searchReq.on('end', function(item) {
         var artist = item.artists.items[0];
         var related = getRelatedArtists(artist.id);
-        
         related.on('end', function(items) {
             artist.related = items.artists;
-            res.json(artist);
+            artist.related.forEach(getArtistsIds);
         });
         
         related.on('error', function(code) {
             res.sendStatus(code);
         });
+        var count = 0;
+        function counter() {
+            count++;
+            if (count === artist.related.length) {
+                res.json(artist);
+            }
+        }
+        
+        
+        function getArtistsIds(element, index) {
+            var topTracks = getRelatedTopTracks(element, function() {
+                counter();
+            });
+            topTracks.on('error', function(code) {
+              res.sendStatus(code); 
+            });
+        }
+        
     });
-
+        
     searchReq.on('error', function(code) {
         res.sendStatus(code);
     });
 });
-
-
 
 app.listen(process.env.PORT || 8080);
